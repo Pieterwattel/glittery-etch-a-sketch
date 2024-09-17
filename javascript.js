33ac230edf479fac393c61ed733971a8de80699e
Pieter_W <163425907+Pieterwattel@users.noreply.github.com>
2024-09-17 17:45:17 +0200

    add a value input and "generate" button
    the value input can let the user decide how many pixels there will be.
    it only accepts numbers,
    and does not accept numbers higher than 100 and lower than 1.

--- javascript.js
+++ javascript.js
@@ -1,12 +1,19 @@
-let input = 7
+let input = document.getElementById("input")
+
+let button = document.getElementById("inputButton")
+
+
+button.addEventListener("click", checkInput)
+
+function checkInput (){
+    if (input.value > 100 || input.value < 1 || isNaN(input.value)){
+        alert ("number was invalid, choose number between 1 and 100")
+    }
+}
+
 let pixelAmount = input * input
 
 const frame = document.getElementById("frame")
-
-
-
-
-
 
 for (i = pixelAmount; i > 0; i--){
     const pixel = document.createElement("span")
@@ -16,5 +23,5 @@ for (i = pixelAmount; i > 0; i--){
 }
 
 function increaseOpacity(pixel){
-    pixel.style.opacity= "100%"
+        pixel.style.opacity= "100%"
 }
