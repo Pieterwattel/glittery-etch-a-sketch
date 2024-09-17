1d15942c312c34ec51c11b58a251bbd1378a9d0a
Pieter_W <163425907+Pieterwattel@users.noreply.github.com>
2024-09-17 17:12:46 +0200

    eventlistener to increase opacity
    there is now an eventlistener for the grid,
    that makes the opacity 100% when the mouse moves over a pixel.

--- javascript.js
+++ javascript.js
@@ -3,9 +3,18 @@ let pixelAmount = input * input
 
 const frame = document.getElementById("frame")
 
+
+
+
+
+
 for (i = pixelAmount; i > 0; i--){
-        const pixel = document.createElement("span")
+    const pixel = document.createElement("span")
     frame.appendChild (pixel);
     pixel.classList.add("pixel");
+    pixel.addEventListener("mouseover", () => increaseOpacity(pixel))
+}
 
+function increaseOpacity(pixel){
+    pixel.style.opacity= "100%"
 }
