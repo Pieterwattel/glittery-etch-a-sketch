12af89685ca7e163dc0e5b0b5e767f2f2709b59a
Pieter_W <163425907+Pieterwattel@users.noreply.github.com>
2024-09-17 17:58:38 +0200

    generate pixels with textinput and button
    the amount of pixels is not specified with user input.
    the generate button will then generate the grid

--- javascript.js
+++ javascript.js
@@ -2,25 +2,32 @@ let input = document.getElementById("input")
 
 let button = document.getElementById("inputButton")
 
-
 button.addEventListener("click", checkInput)
 
 function checkInput (){
     if (input.value > 100 || input.value < 1 || isNaN(input.value)){
         alert ("number was invalid, choose number between 1 and 100")
+    } else {
+        createPixels()
     }
 }
 
-let pixelAmount = input * input
+
+let pixelAmount
+
+function createPixels (){
+    pixelAmount = input.value * input.value
+    for (i = pixelAmount; i > 0; i--){
+        const pixel = document.createElement("span")
+        frame.appendChild (pixel);
+        pixel.classList.add("pixel");
+        pixel.addEventListener("mouseover", () => increaseOpacity(pixel))
+        pixel.style.minWidth= `min(calc(95vw/${input.value}), calc(95vh/${input.value}))`
+    }
+    
+}
 
 const frame = document.getElementById("frame")
-
-for (i = pixelAmount; i > 0; i--){
-    const pixel = document.createElement("span")
-    frame.appendChild (pixel);
-    pixel.classList.add("pixel");
-    pixel.addEventListener("mouseover", () => increaseOpacity(pixel))
-}
 
 function increaseOpacity(pixel){
         pixel.style.opacity= "100%"
